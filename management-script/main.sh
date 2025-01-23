3ae985eb5a45b29f06acab5491531c6e6881492a
Daniel Most <daniel.most@dmostlab.com>
2025-01-22 19:13:24 -0500

    fixed variables and added ip san

--- management-script/main.sh
+++ management-script/main.sh
@@ -16,8 +16,35 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+# Collect variables
+read -p "Enter domain for CA common name (used for generating CA certificates): " DOMAIN_NAME
+if [ -z "$DOMAIN_NAME" ]; then
+    log "Domain name is required"
+    exit 1
+fi
+
+# Extract the domain without the TLD
+DOMAIN=$(echo "$DOMAIN_NAME" | awk -F. '{OFS="."; NF--; print}')
+
+read -p "Enter the hostname for the SSL certificate (used for Vault's HTTPS configuration): " HOSTNAME
+if [ -z "$HOSTNAME" ]; then
+    log "Hostname is required"
+    exit 1
+fi
+
+HOST_IP=$(hostname -I | awk '{print $1}')
+log "Detected host IP: $HOST_IP"
+
+
 # Make scripts executable
 chmod +x install_dependencies.sh setup_vault.sh setup_portainer.sh configure_vault_ca.sh configure_vault.sh
+
+
+# Export variables for use in other scripts
+export DOMAIN_NAME
+export HOSTNAME
+export HOST_IP
+
 # Run the scripts in order
 ./install_dependencies.sh
 ./setup_vault.sh
